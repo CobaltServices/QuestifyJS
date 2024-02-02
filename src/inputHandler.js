482c2947c37cec377b6821e6df4ec3cb6c9b65a0
Hero Link 6 <123016274+HeroLink6@users.noreply.github.com>
2024-02-02 11:12:49 -0800

    Create inputHandler.js

---
+++ src/inputHandler.js
@@ -0,0 +1,22 @@
+// src/inputHandler.js
+
+class InputHandler {
+  constructor() {
+    this.keyPressed = null;
+    this.mouseClicked = false;
+
+    // Set up event listeners
+    document.addEventListener('keydown', this.handleKeyDown.bind(this));
+    document.addEventListener('click', this.handleMouseClick.bind(this));
+  }
+
+  handleKeyDown(event) {
+    this.keyPressed = event.key;
+  }
+
+  handleMouseClick() {
+    this.mouseClicked = true;
+  }
+}
+
+module.exports = InputHandler;
